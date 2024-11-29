f168a647bf4ba474139751d29fe759585343b698
tactkimura <tactkimura_develop@icloud.com>
2024-11-29 23:38:08 +0900

    eventsのプライマリーキーをUUIDに変更

--- database/migrations/2024_11_26_130554_create_questions_table.php
+++ database/migrations/2024_11_26_130554_create_questions_table.php
@@ -8,13 +8,15 @@ class CreateQuestionsTable extends Migration
 {
     public function up()
     {
-        Schema::create('questions', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('event_id')->constrained('events')->onDelete('cascade');
-            $table->string('text')->comment('質問文');
-            $table->enum('type', ['multiple_choice', 'text_input'])->comment('質問の種類');
-            $table->timestamps();
-        });
+        if (Schema::hasTable('events') && !Schema::hasTable('questions')) {
+            Schema::create('questions', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('event_id')->constrained('events')->onDelete('cascade');
+                $table->string('text')->comment('質問文');
+                $table->enum('type', ['multiple_choice', 'text_input'])->comment('質問の種類');
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
--- database/migrations/2024_11_26_130630_create_choices_table.php
+++ database/migrations/2024_11_26_130630_create_choices_table.php
@@ -8,13 +8,15 @@ class CreateChoicesTable extends Migration
 {
     public function up()
     {
-        Schema::create('choices', function (Blueprint $table) {
-            $table->id();
-            $table->foreignId('question_id')->constrained('questions')->onDelete('cascade'); // 外部キー
-            $table->string('text')->comment('選択肢');
-            $table->boolean('is_correct')->default(false)->comment('正解フラグ');
-            $table->timestamps();
-        });
+        if (Schema::hasTable('questions') && !Schema::hasTable('choices')) {
+            Schema::create('choices', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('question_id')->constrained('questions')->onDelete('cascade'); // 外部キー
+                $table->string('text')->comment('選択肢');
+                $table->boolean('is_correct')->default(false)->comment('正解フラグ');
+                $table->timestamps();
+            });
+        }
     }
 
     public function down()
