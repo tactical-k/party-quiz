857a24375f48a045d4b3a1c4d1534f3cdd1f9415
tactkimura <tactkimura_develop@icloud.com>
2024-11-29 22:23:19 +0900

    firebaseのクレデンシャルをenvから読み込むように変更

--- app/Http/Controllers/QuizController.php
+++ app/Http/Controllers/QuizController.php
@@ -9,7 +9,19 @@ class QuizController extends Controller
 {
     public function setQuestion(): JsonResponse
 {
-    $firebase = (new Factory)->withServiceAccount(__DIR__.'/../../../firebase_credentials.json');
+    $firebase = (new Factory)->withServiceAccount([
+        'type' => env('FIREBASE_TYPE'),
+        'project_id' => env('FIREBASE_PROJECT_ID'),
+        'private_key_id' => env('FIREBASE_PRIVATE_KEY_ID'),
+        'private_key' => str_replace('\\n', "\n", env('FIREBASE_PRIVATE_KEY')),
+        'client_email' => env('FIREBASE_CLIENT_EMAIL'),
+        'client_id' => env('FIREBASE_CLIENT_ID'),
+        'auth_uri' => env('FIREBASE_AUTH_URI'),
+        'token_uri' => env('FIREBASE_TOKEN_URI'),
+        'auth_provider_x509_cert_url' => env('FIREBASE_AUTH_PROVIDER_CERT_URL'),
+        'client_x509_cert_url' => env('FIREBASE_CLIENT_CERT_URL'),
+        'universe_domain' => env('FIREBASE_UNIVERSE_DOMAIN'),
+    ]);
     $database = $firebase->withDatabaseUri(env('VITE_FIREBASE_DATABASE_URL'))->createDatabase();
     $database->getReference('currentQuestion')->set([
         'text' => '3 + 2 は何ですか？',         
