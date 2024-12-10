fad900735926df5c01cf06f1e057d66430b656f6
tactkimura <tactkimura_develop@icloud.com>
2024-12-11 00:08:17 +0900

    答え合わせを実装

--- routes/web.php
+++ routes/web.php
@@ -41,6 +41,7 @@ Route::middleware('auth')->group(function () {
   Route::get('/setSampleQuestion', [QuizMasterController::class, 'setSampleQuestion'])->name('set-sample-question');
   Route::get('/quizMaster/{event_id}', [QuizMasterController::class, 'quizMaster'])->name('quiz-master');
   Route::post('/submitQuestion/{question_id}', [QuizMasterController::class, 'submitQuestion'])->name('submit-question');
+  Route::post('/displayAnswer/{question_id}', [QuizMasterController::class, 'displayAnswer'])->name('display-answer');
   Route::post('/clearQuestion/{event_id}', [QuizMasterController::class, 'clearQuestion'])->name('clear-question');
   Route::get('/summary/{event_id}', [QuizMasterController::class, 'summary'])->name('summary');
 });
