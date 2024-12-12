3407af42ab7f2c846bbe96004fb88880ea2b2d00
tactkimura <tactkimura_develop@icloud.com>
2024-12-13 02:30:10 +0900

    favicon設定

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -10,6 +10,8 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <!-- Favicon -->
+        <link rel="icon" href="{{ asset('favicon.ico') }}" type="image/x-icon">
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
